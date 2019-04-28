eb0986a06943184113fd95f8d4ce13f54de012c2
fiiniefi <marcin.drewniak1@vp.pl>
2019-04-28 20:14:47 +0200

    Write every post to proper file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,28 @@
 package main;
 import api.JSONPlaceholderAPI;
-import java.io.IOException;
-import java.io.StringReader;
+
+import java.io.*;
 import javax.json.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        var js = new JSONPlaceholderAPI("https://jsonplaceholder.typicode.com/");
-        //System.out.print(js.get_posts());
+        var postsJson = getPostsJson(
+                new JSONPlaceholderAPI("https://jsonplaceholder.typicode.com/")
+        );
 
-        //JsonReader jsonReader = Json.createReader(new StringReader(posts.toString()));
-        //JsonArray postsJson = jsonReader.readArray();
-        //jsonReader.close();
+        if(new File("posts").mkdir()) {
+            for (JsonValue post : postsJson) {
+                JsonValue postID = ((JsonObject) post).get("id");
+                FileOutputStream stream = new FileOutputStream("posts/post_" + postID + ".txt");
+                stream.write(post.toString().getBytes());
+            }
+        }
+    }
+
+    private static JsonArray getPostsJson(JSONPlaceholderAPI api) throws IOException {
+        JsonReader jsonReader = Json.createReader(new StringReader(api.get_posts()));
+        JsonArray postsJson = jsonReader.readArray();
+        jsonReader.close();
+        return postsJson;
     }
 }
